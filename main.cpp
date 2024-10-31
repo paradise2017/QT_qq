e4606a5d5870c79728b6b02e6f45847cbda30abe
paradise2017 <1131766738@qq.com>
2024-10-31 21:21:40 +0800

    修改CMakeLists版本

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
-﻿//#include "CCMainWindow.h"
-#include "userlogin.h"
+#include "QtQQ_Server.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-   /* CCMainWindow w;
-    w.show();*/
-    //设置当窗口关闭时，不退出
-    a.setQuitOnLastWindowClosed(false);
-
-    UserLogin* userLogin = new UserLogin;
-    userLogin->show();
+    QtQQ_Server w;
+    w.show();
     return a.exec();
 }
--- x64/Debug/qmake/temp/qtvars.pro
+++
@@ -1,2 +0,0 @@
-CONFIG += no_fixpath
-QT += core xml sql network gui widgets webchannel webengine webenginewidgets
